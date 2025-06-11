68b0793d658771a04dc1a6ed2353a04956f07cfa
Owais-Eltigani <ambidextrous.in@gmail.com>
2025-06-11 18:55:36 +0530

    finished the sign in/up screens

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,63 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import CustomButton from '@/components/CustomButton';
+import InputField from '@/components/InputField';
+import OAuth from '@/components/Oauth';
+import { icons, images } from '@/constants';
+import { Link } from 'expo-router';
+import React, { useState } from 'react';
+import { Image, ScrollView, Text, View } from 'react-native';
 
 export default function SignIn() {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+  //   console.log('(auth)/sign-up)', { form });
+
+  const onSignInPress = () => {};
   return (
-    <SafeAreaView>
-      <Text>SignIn</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="relative ">
+        <Image source={images.signUpCar} className="w-full h-[250px] " />
+        <Text className="font-JakartaExtraBold absolute bottom-10 text-3xl ml-5">
+          Welcome 👋
+        </Text>
+      </View>
+
+      <View className="px-5 mt-0 ">
+        {/*  */}
+
+        <InputField
+          label="Email"
+          placeholder="Enter Your Email"
+          icon={icons.email}
+          secureTextEntry={false}
+          value={form.email}
+          onChangeText={value => setForm({ ...form, email: value })}
+        />
+        {/*  */}
+        <InputField
+          label="Password"
+          placeholder="Enter Your Password"
+          icon={icons.lock}
+          secureTextEntry={true}
+          value={form.password}
+          onChangeText={value => setForm({ ...form, password: value })}
+        />
+        <CustomButton
+          title="Sign Up"
+          onPress={onSignInPress}
+          className="mt-5"
+        />
+      </View>
+
+      <View className="px-5 flex ">
+        <OAuth />
+        <Link href={'/(auth)/sign-up'} className="text-center font-semibold">
+          <Text>Do not have Account? </Text>
+          <Text className="color-primary-500">Sign Up</Text>
+        </Link>
+      </View>
+      {/* verification  */}
+    </ScrollView>
   );
 }
